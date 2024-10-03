41f04ce53efe551941271ad8e4e69cbe181e1478
0YOVEK0 <112912353+0YOVEK0@users.noreply.github.com>
2024-10-03 06:19:20 -0600

    1.0
    After 2 repositories we finally get version 1.0

---
+++ GalvanEngine/include/BaseApp.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "Prerequisites.h"
+#include "Window.h"
+#include "ShapeFactory.h"
+#include "ECS/Actor.h"
+
+class
+BaseApp {
+public:
+    BaseApp() = default;
+    ~BaseApp() = default;
+
+    // Funcion que corre el programa en main
+    int
+    run();
+
+    // Funcion de inicializacion
+    bool
+    initialize();
+
+    // Funcion que se actualiza por frame
+    void
+    update();
+
+    // Funcion de renderizado
+    void
+    render();
+
+    // Funcion de liberacion de memoria
+    void
+    cleanup();
+
+private:
+    sf::Clock clock;
+    sf::Time deltaTime;
+
+    Window* m_window;
+    EngineUtilities::TSharedPointer<Actor> Triangle;
+    EngineUtilities::TSharedPointer<Actor> Circle;
+
+    // NUEVO CODIGO DE PRACTICA
+    std::vector<sf::Vector2f> m_trianglePoints;
+    int m_currentTargetIndex;
+    float m_speed;
+};
